32a0671c7ee06822f3bb92b2e255234455320a2d
Joel Mario J <109203750+Joel0302@users.noreply.github.com>
2025-12-30 14:33:51 +0530

    Change depends_on_past to True in DAG defaults

--- dags/test.py
+++ dags/test.py
@@ -4,7 +4,7 @@ from airflow.decorators import dag, task
 # Define default arguments for the DAG
 default_args = {
     'owner': 'airflow',
-    'depends_on_past': False,
+    'depends_on_past': True,
     'retries': 1,
     'retry_delay': timedelta(minutes=5),
 }
