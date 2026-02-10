632e88aade016a146e8eb7414e0f8a19b1673404
Joel Mario J <109203750+Joel0302@users.noreply.github.com>
2026-02-10 18:30:14 +0530

    Update upload_content_to_s3.py

--- devops/upload_content_to_s3.py
+++ devops/upload_content_to_s3.py
@@ -5,18 +5,18 @@ import sys
 
 from botocore.exceptions import ClientError
 
+
 sys.path.append(os.path.join(os.path.dirname(__file__), "../temp_dags"))
 # Creating S3 Resource From the Session.
+#print(os.environ)
 key = os.environ.get("AWS_ACCESS_KEY_ID")
 pwd = os.environ.get("AWS_SECRET_ACCESS_KEY")
 s3bucket = os.environ.get("AWS_S3_BUCKET")
-
+#print(key)
 s3_client = boto3.client("s3", region_name="us-east-1", aws_access_key_id=key, aws_secret_access_key=pwd)
 
 directory = "temp_dags"
 for filename in os.listdir(directory):
-    if filename == "delete_list.txt":
-        continue
     f = os.path.join(directory, filename)
     # checking if it is a file
     if os.path.isfile(f):
@@ -27,50 +27,34 @@ for filename in os.listdir(directory):
             print(e)
         except FileNotFoundError as e:
             print(e)
-
 directory = "temp_dags/utils"
-if os.path.exists(directory):
-    for filename in os.listdir(directory):
-        f = os.path.join(directory, filename)
-        # checking if it is a file
-        if os.path.isfile(f):
-            print(f)
-            try:
-                response = s3_client.upload_file(f, s3bucket, "dags/utils/" + filename)
-            except ClientError as e:
-                print(e)
-            except FileNotFoundError as e:
-                print(e)
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    # checking if it is a file
+    if os.path.isfile(f):
+        print(f)
+        try:
+            response = s3_client.upload_file(f, s3bucket, "dags/utils/" + filename)
+        except ClientError as e:
+            print(e)
+        except FileNotFoundError as e:
+            print(e)
 
 directory = "temp_dags/sql"
-if os.path.exists(directory):
-    for root, sub_dirname, filenames in os.walk(directory):
-        for file in filenames:
-            print(root)
-            print(sub_dirname)
-            print(file)
-            f = os.path.join(root, file)
-            # checking if it is a file
-            if os.path.isfile(f):
-                print(f)
-            try:
-                filekey = f.replace(directory, "")
-                print(filekey)
-                response = s3_client.upload_file(f, s3bucket, "dags/sql" + filekey)
-            except ClientError as e:
-                print(e)
-            except FileNotFoundError as e:
-                print(e)
-
-# Process deletions for files marked with retd_
-delete_list = "temp_dags/delete_list.txt"
-if os.path.exists(delete_list):
-    with open(delete_list, "r") as f:
-        for line in f:
-            target_key = line.strip()
-            if target_key:
-                try:
-                    s3_client.delete_object(Bucket=s3bucket, Key=target_key)
-                    print(f"Deleted from S3: {target_key}")
-                except Exception as e:
-                    print(f"Delete failed for {target_key}: {e}")
+for root,dirname,filename in os.walk(directory):
+    for file in filename:
+        print(root)
+        print(dirname)
+        print(file)
+        f = os.path.join(root, file)
+         # checking if it is a file
+        if os.path.isfile(f):
+            print(f)
+        try:
+            filekey=f.replace(directory,"")
+            print(filekey)
+            response = s3_client.upload_file(f, s3bucket, "dags/sql" +filekey)
+        except ClientError as e:
+            print(e)
+        except FileNotFoundError as e:
+            print(e)
